275ab2ae0fe326f50991d1e9ae015e34ae5c920b
Csingh26 <singh.chaittanya@gmail.com>
2024-12-07 12:17:11 +0530

    db-conn-with-prisma

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,13 +1,15 @@
-const mong = require('mongoose')
+const { PrismaClient} = require("@prisma/client")
 
 require('dotenv').config({
     path: '/Users/chaitanyasingh/Documents/Project/quiz-app/backend/.env'
 }) //configure your env and enter approraite path
 
+const prisma = new PrismaClient()
+
 const connectDB = async () => {
     try {
-        const conn = await mong.connect(process.env.MONGO_URL)
-        console.log('MongoDB connected')
+        const conn = prisma.$connect()
+        console.log('MongoDB connected through Prisma')
 
         return { conn }
     } catch (err) {
