7cf044c9c8f0353d29d76c3e26a33f508be1aab6
Csingh26 <singh.chaittanya@gmail.com>
2024-12-07 09:53:23 +0530

    room-scheduling-feature-added

--- backend/controller/instructor/roomController.js
+++ backend/controller/instructor/roomController.js
@@ -23,20 +23,40 @@ const createRoom = async (req, res) => {
 
         const roomCode = Math.random.toString(36).substring(2, 8).toUpperCase()
 
-        const newRoom = await prisma.activeRoom.create({
-            data: {
-                roomName,
-                roomCode,
-                testModuleId: testModule.id,
-                startTime: new Date(`${expiryDate}T${startTime}`),
-                endTime: new Date(`${expiryDate}T${endTime}`)
-            }
-        })
+        const parsedStartTime = new Date(`${expiryDate}T${startTime}`)
+        const parsedEndTime = new Date(`${expiryDate}T${endTime}`)
 
-        res.status(201).json({
-            message: "Room created successfully",
-            roomCode
-        })
+        const roomData = {
+            roomName,
+            roomCode,
+            testModule: {
+                connect: {
+                    id: testModule.id
+                }
+            },
+            startTime: parsedStartTime,
+            endTime: parsedEndTime
+        }
+
+        if (parsedStartTime > new Date()) {
+            const futureRoom = await prisma.scheduledRoom.create({
+                data: roomData
+            })
+
+            res.status(200).json({
+                message: "Room scheduled Successfully",
+                roomCode,
+            })
+        } else {
+            const newRoom = await prisma.activeRoom.create({
+                data: roomData
+            })
+    
+            res.status(201).json({
+                message: "Room created successfully",
+                roomCode,
+            })
+        }
     } catch (err) {
         console.error("Error creating room:", err)
         res.status(500).json({
@@ -77,4 +97,35 @@ const transferExpiredRooms = async () => {
     }
 }
 
-module.exports = { createRoom, transferExpiredRooms}
+const activateScheuledRooms = async () => {
+    try {
+        const now = new Date()
+
+        const roomsToActivate = await prisma.scheduledRoom.findMany({
+            where: {
+                startTime: { lte: now }
+            }
+        })
+
+        for (const room of roomsToActivate) {
+            await prisma.activeRoom.create({
+                data: {
+                    roomName: room.roomName,
+                    roomCode: room.roomCode,
+                    testModuleId: room.testModuleID,
+                    startTime: room.startTime,
+                    endTime: room.endTime
+                }
+            })
+
+            await prisma.scheduledRoom.delete({
+                where: { id: room.id }
+            })
+        }
+        console.log(`Activated ${roomsToActivate.length} scheduled rooms`)
+    } catch (err) {
+        console.log("Error activating scheduled rooms:", err)
+    }
+}
+
+module.exports = { createRoom, transferExpiredRooms, activateScheuledRooms}
--- backend/routes/instructor/roomTransfer.js
+++ backend/routes/instructor/roomTransfer.js
@@ -1,10 +1,18 @@
 const cron = require("node-cron")
-const { transferExpiredRooms } = require("../../controller/instructor/roomController")
+const { transferExpiredRooms, activateScheuledRooms } = require("../../controller/instructor/roomController")
 
 const scheduleRoomTransfer = () => {
     cron.schedule("* * * * *", async () => {
         console.log("Checking for expired rooms...")
-        await transferExpiredRooms()
+        try {
+            await activateScheuledRooms()
+
+            await transferExpiredRooms()
+
+            console.log("Room cheks and transfer completed.")
+        } catch (err) {
+            console.log("Error in room transfer cron job", err)
+        }
     })
 }
 
