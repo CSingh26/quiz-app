cda1fb7bed431c9d6fbd872ceb00164239f44df4
Csingh26 <singh.chaittanya@gmail.com>
2024-12-17 03:22:07 +0530

    synced-file-for-env-vars

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -6,10 +6,14 @@ require('dotenv').config({
     path: '/Users/chaitanyasingh/Documents/Project/quiz-app/backend/.env'
 })
 
+console.log("Bucket Name:", process.env.AWS_BUCKET_NAME);
+console.log("Region:", process.env.AWS_REGION_NAME);
+console.log("Access Key:", process.env.AWS_ACCESS_KEY)
+
 const s3Client = new S3Client({
-    region: process.env.AWS_REGION,
+    region: process.env.AWS_REGION_NAME,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        accessKeyId: process.env.AWS_ACCESS_KEY,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     }
 })
@@ -23,18 +27,17 @@ const upload = multer({
 const uploadToS3 = async (file, folder) => {
     const fileName = `${folder}/${uuidv4()}-${file.originalname}`
     const params = {
-        Bucker: process.env.AWS_BUCKET_NAME,
+        Bucket: process.env.AWS_BUCKET_NAME,
         Key: fileName,
         Body: file.buffer,
         ContentType: file.mimetype,
-        ACL: "public-read"
     }
 
     try {
         const command = new PutObjectCommand(params)
         await s3Client.send(command)
 
-        const fileUrl = `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${fileName}`
+        const fileUrl = `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION_NAME}.amazonaws.com/${fileName}`
         return fileUrl
     } catch (err) {
         console.error("Error uploading to S3:", err)
