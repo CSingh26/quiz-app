ab010e072f136e8d2ea092b9ec5e5d29e175bee2
Csingh26 <singh.chaittanya@gmail.com>
2024-12-16 11:29:28 +0530

    sidebar-items-managed

--- frontend/src/app/dashboard/components/Sidebar.tsx
+++ frontend/src/app/dashboard/components/Sidebar.tsx
@@ -4,18 +4,40 @@ import React from "react"
 import SidebarItem from "./SidebarItem"
 
 const Sidebar = () => {
-    return (
-        <div className="h-screen w-20 bg-[#2d2e73] flex flex-col justify-between items-center py-6">
-            <div className="space-y-8">
-                <SidebarItem icon="/Assests/Icons/user.png" href="/dashboard/student/profile" alt="Profile" active/>
-                <SidebarItem icon="/Assests/Icons/activeRoom.png" href="/dashboard/student/activeRooms" alt="Active Room" active/>
-                <SidebarItem icon="/Assests/Icons/pastRoom.png" href="/dashboard/student/pastRooms" alt="Past Quizzes" active/>
-            </div>
-            <div>
-            <SidebarItem icon="/Assests/Icons/logout.png" href="/dashboard/student/logout" alt="Logout" active/>
-            </div>
-        </div>
-    )
+  return (
+    <div className="h-screen w-20 bg-[#2d2e73] flex flex-col justify-between items-center py-6">
+
+      <div className="flex flex-col items-center gap-10 mt-auto">
+        <SidebarItem
+          icon="/Assests/Icons/user.png"
+          href="/dashboard/student/profile"
+          alt="Profile"
+          active
+        />
+        <SidebarItem
+          icon="/Assests/Icons/activeRoom.png"
+          href="/dashboard/student/activeRooms"
+          alt="Active Room"
+          active
+        />
+        <SidebarItem
+          icon="/Assests/Icons/pastRoom.png"
+          href="/dashboard/student/pastRooms"
+          alt="Past Quizzes"
+          active
+        />
+      </div>
+
+      <div className="mt-auto">
+        <SidebarItem
+          icon="/Assests/Icons/logout.png"
+          href="/dashboard/student/logout"
+          alt="Logout"
+          active
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Sidebar
