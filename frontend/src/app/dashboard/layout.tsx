91b1e87b4280368a5e11b98accde5285442e04bd
Csingh26 <singh.chaittanya@gmail.com>
2024-12-19 02:23:37 +0530

    components-moved

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import Sidebar from "./components/Sidebar"
+import Sidebar from "./student/components/Sidebar"
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
