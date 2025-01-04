e5b684eeb5d9f9a67b53c92cc813f1c31a63c97f
Csingh26 <singh.chaittanya@gmail.com>
2025-01-04 23:16:30 +0530

    home-page-ui-change-initialized

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,23 +4,6 @@ import Link from "next/link"
 
 export default function Page() {
     return (
-        <div className="bg-[#79bac1] h-screen flex flex-col justify-center items-center custom-font">
-        <div className="bg-[#aeddcc] p-8 rounded-lg shadow-md flex flex-col items-center justify-center w-4/5 h-4/5">
-          <h1 className="text-6xl font-bold mb-10">Welcome to Grass Quiz Portal</h1>
-          <h2 className="text-3xl mb-10">Please choose from the below options</h2>
-          <div className="flex gap-6">
-            <Link href={'/login/student'}>
-              <button className="text-2xl font-bold px-6 py-2 bg-[#79bac1] text-white rounded-md hover:bg-[#2a7886] transition">
-                Student
-              </button>
-            </Link>
-            <Link href={'/login/instructor'}>
-              <button className="text-2xl font-bold px-6 py-2 bg-[#79bac1] text-white rounded-md hover:bg-[#2a7886] transition">
-                Instructor
-              </button>
-            </Link>
-          </div>
-        </div>
-      </div>
+        <div></div>
     )
 }
